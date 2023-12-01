7026d8a3635e62ff6d9aefbde5bd3b66876cb248
Alexandr Vrána <43996537+PainDesu@users.noreply.github.com>
2023-12-01 14:20:07 +0100

    Part2

--- Day1/main.py
+++ Day1/main.py
@@ -2,8 +2,21 @@ f = open("input.txt", "r")
 lines = f.readlines()
 f.close()
 ans = 0
+dict = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "t3e",
+    "four": "f4r",
+    "five": "f5e",
+    "six": "s6x",
+    "seven": "s7n",
+    "eight": "e8t",
+    "nine": "n9e"
+}
 
 for line in lines:
+    for a in dict:
+        line = line.replace(a, dict[a])
     first = "a"
     last = "a"
     for char in line:
